10077c1dd4544f41c396030a54ca1b7e8a103c88
Tate Stone <tatejamesstone@gmail.com>
2019-09-25 10:27:03 -0600

    Adding store and setting up redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store'
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
